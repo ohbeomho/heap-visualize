857c7c92f8702dfe3ed3986da260b8f138d52ace
ohbeomho <ohbeomho0322@gmail.com>
2026-04-29 18:47:08 +0900

    WIP: 트리 표현 먼저 하기

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
-const div_nodes = document.querySelector(".nodes")
+const nodesDiv = document.querySelector('.nodes')
+const keyInput = document.querySelector('input#key'),
+    addKeyButton = document.querySelector('button#add')
+
+const nodes = [null]
+let last = 0
 
 class HeapNodeElement {
-    constructor(key, left, right) {
+    constructor(key) {
         this.key = key
-        this.element = document.createElement("div")
+        this.element = document.createElement('div')
+        this.element.classList.add('heap-node')
         this.element.innerText = key
-        div_nodes.appendChild(this.element)
-
-        this.left = left
-        this.right = right
+        nodesDiv.appendChild(this.element)
     }
 
     static swap(target) {
@@ -20,3 +23,29 @@ class HeapNodeElement {
         target.innerText = target.key
     }
 }
+
+const MAX_NODES = 1024
+const BASE_Y = 50
+const FLOOR_HEIGHT = 100
+
+// 2의 제곱번째 노드가 추가될 때마다 층이 추가됨
+const floors = {
+    1: true,
+}
+let curr = 2
+while (curr <= MAX_NODES) {
+    floors[curr] = true
+    curr = curr * 2
+}
+
+let currFloor = 0
+function addKey() {
+    const key = keyInput.valueAsNumber
+    if (isNaN(key)) return
+
+    nodes[++last] = new HeapNodeElement(key)
+    if (floors[last]) currFloor++
+    nodes[last].style.top = `${BASE_Y + (currFloor - 1) * FLOOR_HEIGHT}px`
+}
+
+addKeyButton.addEventListener('click', addKey)
